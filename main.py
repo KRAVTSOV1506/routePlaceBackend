9ec3bf897033e1f7b12c82be3f92bea76b0d431f
Dmitriy Kravtsov <dkravstov@coinweb.io>
2023-06-20 01:35:54 +0300

    feat: getTokenDataByTokenId

--- main.py
+++ main.py
@@ -132,6 +132,53 @@ def get_listed_tokens():
     return json.dumps(tokens)
 
 
+@post("/getTokenDataByTokenId")
+def get_token_data():
+    token = connection.select(
+        '''
+            SELECT 
+                t.chain_id,
+                t.collection_address,
+                t.token_id,
+                t.token_uri,
+                t.name,
+                t.image,
+                t.owner,
+                t.desc,
+                o.uuid AS order_uuid
+            FROM tokens t
+            LEFT JOIN orders o
+                ON o.chain_id = t.chain_id AND
+                   o.collection_address = t.collection_address AND 
+                   o.token_id = t.token_id
+            LEFT JOIN contracts c
+                ON t.collection_address = c.address
+            WHERE c.collection = %s AND t.token_id = %s
+            ORDER BY o.nonce DESC
+            LIMIT 1;
+        ''',
+        (request.json["collectionUuid"], request.json["tokenId"])
+    )[0]
+
+    if token["order_uuid"]:
+        token["prices"] = get_token_prices(token["order_uuid"])
+
+    token["properties"] = [
+        {
+            "trait_type": "Hair",
+            "value": "Red",
+        },
+        {
+            "trait_type": "Body",
+            "value": "Blond",
+        }
+    ]
+
+    token["history"] = []
+
+    return token
+
+
 @post("/getTokenData")
 def get_token_data():
     token = connection.select(
